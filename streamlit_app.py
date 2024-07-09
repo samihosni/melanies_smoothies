a0de08eb547084c752c473e2e21e71d8377f8eef
samihosni <128069585+samihosni@users.noreply.github.com>
2024-07-09 14:49:44 +0100

    importing requests
    The requests library allows us to build and sent REST API calls.  Paste the code below into the bottom of your SniS app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 # Import python packages
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 import streamlit as st
 from snowflake.snowpark.functions import col
 cnx = st.connection("snowflake")
