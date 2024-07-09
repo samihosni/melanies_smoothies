b261584103b53ffa2927e2997dd4d537a8d17660
samihosni <128069585+samihosni@users.noreply.github.com>
2024-07-09 15:52:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.dataframe(pd_df)
 #st.stop()
 ingredients_list= st.multiselect(
     'Choose up to 5 ingredients :',
-    my_dataframe
+    pd_df
     ,max_selection=5
     )
 if ingredients_list:
